ec6d79fedf3a9513241f68bdc44a6b732320560e
Christopher Diamana Lutete <christopher.diamana-lutete@epita.fr>
2021-11-01 17:41:33 +0100

    [SERVER][UPADATE] Started the implemention of some methods

--- src/server.cc
+++ src/server.cc
@@ -1,10 +1,12 @@
 #include "server.hh"
 
+#include "message.hh"
+
 
 Server::Server(unsigned long rank, unsigned long size)
   : rank(rank)
     , size(size)
-    , status(Folower)
+    , state(Status::Follower)
     , currentTerm(0)
     , timeout()
     , heartbeatTimeout(50)
@@ -15,7 +17,8 @@ Server::Server(unsigned long rank, unsigned long size)
     , log()
 {
     setTimeout();
-    LOG(DEBUG) << "server " << rank_ << "has PID " << getpid();
+    setLog();
+    LOG(DEBUG) << "server " << rank << "has PID " << getpid();
 }
 
 void Server::setTimeout(){
@@ -23,8 +26,25 @@ void Server::setTimeout(){
   this->heartbeatTimeout = INTIAL_HARTBEAT
 }
 
- void Server::setLog(){
-   filename = "server_" + std::to_string(rank) + ".log";
-   this->log.open(filename);
- }
+void Server::setLog(){
+  filename = "bin/server_" + std::to_string(rank) + ".log";
+  this->log.open(filename);
+}
 
+void Server::start(){
+  if (this->state == Status::Follower)
+    this->toCandidate()
+}
+
+
+void Server::toCandidate(){
+  // Update attributes
+  this->state = Status::Candidate;
+  this->votedFor = this->rank;
+  this->currentTerm += 1;
+
+  int voteCounter = 1;
+
+  // Vote request
+  requestVote()
+}
